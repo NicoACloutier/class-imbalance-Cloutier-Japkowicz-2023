1777bceef168ae0e87c60c341794433113fb41e0
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-10-25 12:12:02 -0400

    document regex

--- consolidate.py
+++ consolidate.py
@@ -4,9 +4,9 @@ import pandas as pd
 def main():
     with open('raw.txt', 'r', encoding='utf8') as f:
         text = f.read()
-        text = re.sub('\n[A-Z]\n', '', text)
-        words = re.findall('(?<=\n).+?(?= \– )', text)
-        definitions = re.findall('(?<= \– ).+?(?=\n)', text)
+        text = re.sub('\n[A-Z]\n', '', text) #delete lines with only single uppercase letter (these lines denote the beginning of a new letter, not useful)
+        words = re.findall('(?<=\n).+?(?= \– )', text) #get all of the words in the text, in the format (\nWORD - ).
+        definitions = re.findall('(?<= \– ).+?(?=\n)', text) #get all of the definitions in the text, in the format ( - DEFINITION\n).
     
     word_series = pd.DataFrame([words, definitions], ["Words", "Definitions"]).transpose()
     word_series.to_csv('words.csv', index=False)
