6dc9d18242b485440a5cd9515118ee954714f116
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-10-27 09:38:25 -0400

    add new words

--- consolidate.py
+++ consolidate.py
@@ -10,13 +10,20 @@ def main():
         words = re.findall('(?<=\n).+?(?= \– )', text) #get words, in format (\nWORD - ).
         definitions = re.findall('(?<= \– ).+?(?=\n)', text) #get definitions, in format ( - DEFINITION\n).
     
+    with open('4chanwords.txt', 'r', encoding='utf8') as f:
+        text = f.read()
+        text = re.sub('\n.(?=\n)', '', text) #delete single-character lines
+        temp_words = re.findall('(?<=\n).+?(?=\n)', text) #get words
+        words += temp_words #add to total words
+        definitions += ['None'] * len(temp_words) #add 'None' definitions to words
+    
     df = pd.read_csv('..\\data.csv') #read data (must be in file named data.csv in parent to current directory)
     
     appearances_list = [] #list of appearances of each word
     averaged_values = {'hateful': [], 'offensive': [],
                        'sentiment': [], 'violent': []} #values to be averaged for each word and recorded in df
     for i, word in enumerate(words):
-        line_df = df[df['text'].str.contains(f'[,\.\s\?]{word}s?[,\.\s\?]', case=False)] #make df of lines with word appearing in it\
+        line_df = df[df['text'].str.contains(f'[,\.\s\?]{word}s?[,\.\s\?]', case=False)] #make df of lines with word appearing in it
         appearances = len(line_df) #get number of appearances for word
         appearances_list.append(appearances)
         if appearances > 0:
@@ -28,9 +35,11 @@ def main():
     definitions = select(definitions, appearances_list, 0)
     appearances_list = select(appearances_list, appearances_list, 0)
     
-    word_df = pd.DataFrame([words, definitions, appearances_list], ["word", "definition", "appearance"]).transpose()
+    word_df = pd.DataFrame([words, definitions, appearances_list], ["word", "definition", "appearances"]).transpose()
     for key in averaged_values:
         word_df[key] = averaged_values[key]
+    
+    word_df = word_df.sort_values('appearances', ascending=False)
     
     word_df.to_csv('words.csv', index=False)
 
