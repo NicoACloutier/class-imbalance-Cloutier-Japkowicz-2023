5a9a6dde5dae9548e4e5748caa9f7ba214f88bcf
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2023-03-24 14:24:32 -0400

    finalize generation script

--- reorg_scripts/generate.py
+++ reorg_scripts/generate.py
@@ -1,12 +1,15 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import transformers
 import numpy as np
 import pandas as pd
+import string
 
 INPUT_FILE = '..\\data\\antisemitism_dataset.csv'
 OUTPUT_FILE = '..\\data\\augmented_dataset.csv'
 NUM_TEMPS = 20
 
-tokenize = lambda text, tokenizer: tokenizer.encode(text, return_tensors='pt')
+tokenize = lambda text, tokenizer: tokenizer.encode(text, max_length=15, truncation=True, return_tensors='pt')
+delete_punctuation = lambda x: ''.join([char for char in x if char not in [string.punctuation]+['â€”&gt;']])
 
 #multiply each item of an array a certain number of times
 def multiply(list, times_over):
@@ -16,19 +19,23 @@ def multiply(list, times_over):
     return array
 
 def main():
+    transformers.utils.logging.set_verbosity_error()
+
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     model = GPT2LMHeadModel.from_pretrained('gpt2')
     
     df = pd.read_csv(INPUT_FILE)
-    original_texts = list(df['text'].astype(str))
+    original_texts = list(df['text'].astype(str).apply(delete_punctuation))
     final_texts = []
     for text in original_texts:
+        length = len(text)
         encoding = tokenize(text, tokenizer)
         for temp in range(NUM_TEMPS):
-            temp += (1/(NUM_TEMPS/2))
             temp /= (NUM_TEMPS/2)
-            generation = model.generate(encoding, max_length=200, do_sample=True, temperature=temp)
+            temp += 1
+            generation = model.generate(encoding, max_length=25, do_sample=True, min_new_tokens=5, length_penalty=1, temperature=temp, top_k=10)
             text = tokenizer.decode(generation[0], skip_special_tokens=True)
+            text = text.replace('\n', ' ')
             final_texts += text
     
     classifications = multiply(list(df['classification'].astype(int)), NUM_TEMPS)
